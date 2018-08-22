26262fcd80f4fd4ef44b2236298974b1bffe83ae
mrmodise <modisemorebodi@gmail.com>
2018-08-22 08:12:11 +0200

    script cleanup

--- clean-convert.sh
+++ clean-convert.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 # Replace /.attachments with the correct path, only in markdown files
-LC_ALL=C find ./ -iname "*.md" -type f -exec sed -i '' -e 's$/.attachments$../.attachments$g' {} \;
+find ./ -iname "*.md" -type f -exec sed -i '' -e 's$/.attachments$../.attachments$g' {} \;
 
 # Remove VSTS image sizing  e.g. =400x, =500x, 1200x 
-LC_ALL=C find ./ -type f -exec sed -i '' -e 's$=[0-9]*x$''$g' {} \;
+find ./ -iname "*.md" -type f -exec sed -i '' -e 's$=[0-9]*x$''$g' {} \;
 
 # Convert markdown files to pdf
 find ./ -path ./.attachments -prune -o -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -f markdown -t latex -s -o "${0%.md}.pdf"' {} \;
+
+# Size images
+
+# Generate Table of Contents
+
+# Combine pdf and ensure correct numbering
